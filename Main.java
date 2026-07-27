8049df84a5c651a87ba9dd46bca5f534153f95a3
Sanjay Thavanash K <sanjaythavanash1614@gmail.com>
2026-07-27 09:49:34 +0530

    1.5

--- Main.java
+++ Main.java
@@ -6,13 +6,14 @@ public class Main{
         System.out.println("Track shared expenses with friends");
         System.out.println();
         System.out.println("Ready. More features coming in the next lessons");
-        String name = "max";
-        System.out.println(name);
+        String payerName = "max";
         double totalamt =500;
-        System.out.println(totalamt);
+        String expLine = payerName + " paid :" + totalamt;
+        System.out.println(expLine);
         int numfrnds = 3;
-        System.out.println(numfrnds);
         double personshare = totalamt/numfrnds;
-        System.out.println(personshare);
+        String shareLine = "each person pays :" + personshare;
+        System.out.println(shareLine);
+
     }
 }
