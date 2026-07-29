64a6239aab95a7ffa5bdbb7dfb4c19a925349a69
Thavanash-31x5 <sanjaythavanash1614@gmail.com>
2026-07-29 07:24:55 +0530

    2.2

--- Friend.java
+++ Friend.java
@@ -4,10 +4,14 @@ public class Friend {
     private String name;
 
     public Friend(String name){
+
         this.name=name;
     }
 
     public String getName(){
+
         return name;
     }
+
+
 }
